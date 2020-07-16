76f82dc6243719c86844a27ef70d4b5a4cd69885
FaizaAS <FaizaAS@192.168.10.10>
2020-07-16 23:55:56 +0500

    My first commit

---
+++ src/test/java/com/PracticeGit/TestScripts/GitPractice.java
@@ -0,0 +1,17 @@
+package com.PracticeGit.TestScripts;
+
+import org.testng.annotations.Test;
+
+public class GitPractice {
+	
+	@Test(priority=1)
+	public void TC01() {
+		
+		System.out.println("This is my first Class");
+		
+		
+	}
+	
+	
+
+}
