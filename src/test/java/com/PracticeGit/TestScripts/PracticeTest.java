2cf1e1478462db5ecd3a0c214c846ca2f2b9c0dc
khadijaasif01 <asifkhadija60@gmail.com>
2020-07-17 00:10:32 -0700

    This is my First Commit.

---
+++ src/test/java/com/PracticeGit/TestScripts/PracticeTest.java
@@ -0,0 +1,14 @@
+package com.PracticeGit.TestScripts;
+
+import org.testng.annotations.Test;
+
+public class PracticeTest {
+
+	@Test(priority=1)
+	public void TC0_Case() {
+		
+		System.out.println("This is my first Commit - Khadija");
+		
+		
+	}
+}
